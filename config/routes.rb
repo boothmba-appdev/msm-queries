794f99b2d6c8809f0afb79dbccb8c1801e159b28
hxie0 <xhy@chicagobooth.edu>
2021-07-22 02:45:25 +0000

    in progress-2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", {:controller => "directors", :action => "index"})
+
+  get("/directors/eldest", {:controller => "directors", :action => "wisest"})
+
+
+
   get("/characters", {:controller => "characters", :action => "index"})
   get("/movies", {:controller => "movies", :action => "index"})
   get("/actors", {:controller => "actors", :action => "index"})
